741531a9e1fd8a7ce8c05e92e4cd0ced916c5a8a
ankurghose <61168478+ankurghose@users.noreply.github.com>
2024-07-15 21:01:00 -0400

    add winner status game play function

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,11 +2,12 @@ console.log("tic-tae-toe")
 
 //state
 //used to track the state of the game
-let turn, board
+let turn, board, winner
 
 
 //game logic
 init()
+gameplayLoop()
 
 //get the game in a starting position 
 function init(){
@@ -16,6 +17,7 @@ function init(){
     " ", " ", " ",
     " ", "o", " "
     ]
+    winner = null 
     render()
 }
 
@@ -26,7 +28,7 @@ function render() {
 }
 function renderBoard(){
      // rendering the board
-     console.log(' 1 |2| 3');
+    // console.log(' 1 |2| 3');
      console.log(`1 ${board[0]}|${board[1]}|${board[2]}`);
      console.log(' --|-|--');
      console.log(`2 ${board[3]}|${board[4]}|${board[5]}`);
@@ -34,4 +36,14 @@ function renderBoard(){
      console.log(`3 ${board[6]}|${board[7]}|${board[8]}`);
 
 }
+//what goes in a turn
+//have to chose empty space
+//put x or o
+//check if we won or not
+//change the turn
 
+function gameplayLoop() {
+    while (!winner) {
+
+    }
+}
